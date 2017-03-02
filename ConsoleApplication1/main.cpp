4320fff56530d29e5f20aa9e722729630fdd48c0
MasterZ1 <mdcrabtree2@gmail.com>
2017-03-02 11:34:01 -0500

    blur improvements with uniform

--- ConsoleApplication1/main.cpp
+++ ConsoleApplication1/main.cpp
@@ -180,6 +180,7 @@ int main(int argc, char* argv[])
 		postprocprog->use();
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		postprocprog->setUniform("tex", fbo1->texture);
+		postprocprog->setUniform("numOfIterations", 4);
 		usq->draw(postprocprog);
 
 		fbo1->texture->unbind();
