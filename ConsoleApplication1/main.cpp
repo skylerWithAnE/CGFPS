9a70bc3876865aaca7a05cf95778bbad6fc9fed0
Skyler Evans <skylerWithAnE@bitbucket.org>
2017-04-18 22:45:06 -0400

    removed some annoying debug stuff, added hotkey for shadowbuffer render swap... lack of shadows stemming from vs. (in some way, idk.)

--- ConsoleApplication1/main.cpp
+++ ConsoleApplication1/main.cpp
@@ -102,7 +102,14 @@ int main(int argc, char* argv[])
     //view camera
     Camera* cam = new Camera();
     cam->look_at(vec3(5,1,5), vec3(10,1,10), vec3(0,1,0) );
-	Camera* lcam = new Camera(0.01f, 50.f, cam->eye);
+	Camera* lightCamera = new Camera();
+	cam->hither = 0.01f;
+	lightCamera->yon = 50.f;
+	lightCamera->walk(0.3f);
+	lightCamera->strafe(6.f, -1.f, 0.f);
+	lightCamera->look_at(vec3(5.3, 0.6, 5.5), vec3(10, 1, 10), vec3(0, 1, 0));
+	
+
 
     //tells which keys are currently down
     set<int> keys;
@@ -118,63 +125,85 @@ int main(int argc, char* argv[])
 
 	bool shadow_buffer_debug = false;
 	float sbdcd = 1.f;
-    while(1){
-        //pump the event queue...
-        while(1){
-            if(!SDL_PollEvent(&ev)){
-                break;
-            }
-        
-            if(ev.type == SDL_QUIT){
-                exit(0);
-            } else if( ev.type == SDL_KEYDOWN ){
-                int k = ev.key.keysym.sym;
-                keys.insert(k);
-                if(k == SDLK_q){
-                    exit(0);
-                }
+	while (1) {
+		//pump the event queue...
+		while (1) {
+			if (!SDL_PollEvent(&ev)) {
+				break;
+			}
 
-            } else if(ev.type == SDL_KEYUP){
-                int k = ev.key.keysym.sym;
-                keys.erase(k);
-            } else if( ev.type == SDL_MOUSEBUTTONDOWN ){
-                cout << "Pew!\n";
-                //-1=any channel, 0=no repeat
-                Mix_PlayChannel(-1,pew,0);
-            } else if(ev.type == SDL_MOUSEBUTTONUP){
-                //mouse up,ev.button.button,ev.button.x,ev.button.y
-            } else if( ev.type == SDL_MOUSEMOTION ){
-                float dx = (float)ev.motion.xrel;
+			if (ev.type == SDL_QUIT) {
+				exit(0);
+			}
+			else if (ev.type == SDL_KEYDOWN) {
+				int k = ev.key.keysym.sym;
+				keys.insert(k);
+				if (k == SDLK_q) {
+					exit(0);
+				}
+
+			}
+			else if (ev.type == SDL_KEYUP) {
+				int k = ev.key.keysym.sym;
+				keys.erase(k);
+			}
+			else if (ev.type == SDL_MOUSEBUTTONDOWN) {
+				cout << "Pew!\n";
+				//-1=any channel, 0=no repeat
+				Mix_PlayChannel(-1, pew, 0);
+			}
+			else if (ev.type == SDL_MOUSEBUTTONUP) {
+				//mouse up,ev.button.button,ev.button.x,ev.button.y
+			}
+			else if (ev.type == SDL_MOUSEMOTION) {
+				float dx = (float)ev.motion.xrel;
 				float dy = (float)ev.motion.yrel;
-				
+
 				cam->turn(-0.01f*dx);
 				cam->tilt(-0.01f*dy);
-            }
-        }
-        
-		
-        int now = SDL_GetTicks();
-        float elapsed = (float)(now-last);
-        last=now;
+				lightCamera->turn(-0.01f*dx);
+				lightCamera->tilt(-0.01f*dy);
+			}
+		}
+
+
+		int now = SDL_GetTicks();
+		float elapsed = (float)(now - last);
+		last = now;
 		sbdcd += elapsed;
-		cout << elapsed << endl;
-        
-        if(keys.find(SDLK_w) != keys.end() )
-            cam->walk(0.002f*elapsed);
-        if(keys.find(SDLK_s) != keys.end() )
-            cam->walk(-0.002f*elapsed);
-        if(keys.find(SDLK_j) != keys.end() )
-            cam->turn(0.001f*elapsed);
-        if(keys.find(SDLK_l) != keys.end() )
-            cam->turn(-0.001f*elapsed);
-        if(keys.find(SDLK_a) != keys.end() )
-            cam->strafe(-0.002f*elapsed,0,0);
-        if(keys.find(SDLK_d) != keys.end() )
-            cam->strafe(0.002f*elapsed,0,0);
-        if(keys.find(SDLK_i) != keys.end() )
-            cam->strafe(0,0.01f*elapsed,0);
-        if(keys.find(SDLK_k) != keys.end() )
-            cam->strafe(0,-0.01f*elapsed,0);
+
+		if (keys.find(SDLK_w) != keys.end()) {
+			cam->walk(0.002f*elapsed);
+			lightCamera->walk(0.002f*elapsed);
+		}
+		if (keys.find(SDLK_s) != keys.end()) {
+			cam->walk(-0.002f*elapsed);
+			lightCamera->walk(-0.002f*elapsed);
+		}
+		if (keys.find(SDLK_j) != keys.end()) {
+			cam->turn(0.001f*elapsed);
+			lightCamera->turn(0.001f*elapsed);
+		}
+		if (keys.find(SDLK_l) != keys.end()) {
+			cam->turn(-0.001f*elapsed);
+			lightCamera->turn(-0.001f*elapsed);
+		}
+		if (keys.find(SDLK_a) != keys.end()) {
+			cam->strafe(-0.002f*elapsed, 0, 0);
+			lightCamera->strafe(-0.002f*elapsed, 0, 0);
+		}
+		if (keys.find(SDLK_d) != keys.end()) {
+			cam->strafe(0.002f*elapsed, 0, 0);
+			lightCamera->strafe(0.002f*elapsed, 0, 0);
+		}
+		if (keys.find(SDLK_i) != keys.end()) {
+			cam->strafe(0, 0.01f*elapsed, 0);
+			lightCamera->strafe(0, 0.01f*elapsed, 0);
+		}
+		if (keys.find(SDLK_k) != keys.end()) {
+			cam->strafe(0, -0.01f*elapsed, 0);
+			lightCamera->strafe(0, -0.01f*elapsed, 0);
+		}
 		if (keys.find(SDLK_SLASH) != keys.end()) {
 			if (sbdcd > 200.f) {
 				shadow_buffer_debug = !shadow_buffer_debug;
@@ -190,14 +219,11 @@ int main(int argc, char* argv[])
 		sbprog->use();
 		if(!shadow_buffer_debug)
 			shadowbuffer->bind();
+		glClearColor(1.f, 1.f, 1.f, 1.f); 
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		lcam->eye = cam->eye + vec4(0.1f, -0.4f, 0.3f, 0.f);
-		lcam->compute_view_matrix();
-		sbprog->setUniform("viewMatrix", lcam->viewmatrix);
-		sbprog->setUniform("projMatrix", lcam->projmatrix);
-		vec3 hyymh = vec3(lcam->hither, lcam->yon, lcam->yon - lcam->hither);
+		vec3 hyymh = vec3(lightCamera->hither, lightCamera->yon, lightCamera->yon - lightCamera->hither);
 		sbprog->setUniform("hitheryon", hyymh);
-		cam->draw(sbprog);
+		lightCamera->draw(sbprog);
 		world->draw(sbprog);
 		world->robotDraw(sbprog);
 		if (!shadow_buffer_debug)
@@ -207,11 +233,11 @@ int main(int argc, char* argv[])
 
 		if (!shadow_buffer_debug) {
 			shadowprog->use();
-			shadowprog->setUniform("lightPos", lcam->eye);
+			shadowprog->setUniform("lightPos", lightCamera->eye);
 			shadowprog->setUniform("lightColor", vec4(1.f, 1.f, 1.f, 1.f));
 			shadowprog->setUniform("hitheryon", hyymh);
 			shadowprog->setUniform("shadowbuffer", shadowbuffer->texture);
-			shadowprog->setUniform("lightDir", (-1 * lcam->W));
+			shadowprog->setUniform("lightDir", (-1 * lightCamera->W));
 			shadowprog->setUniform("magicConst", 15.0f);
 			shadowprog->setUniform("scaleFactor", 5.0f);
 			cam->draw(shadowprog);
